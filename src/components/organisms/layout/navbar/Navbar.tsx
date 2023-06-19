f8a662eada2befd734260e8b2df4d27d628ef772
DESKTOP-AGFU7TK\Pablo <pabloriusblanco@gmail.com>
2023-06-19 13:49:42 -0300

    Fix - minor Imports fixes

--- src/components/organisms/layout/navbar/Navbar.tsx
+++ src/components/organisms/layout/navbar/Navbar.tsx
@@ -1,12 +1,10 @@
 import { useAuth } from "../../../../hooks/useAuth";
 import useModal from "../../../../hooks/useModal";
-import { useSpinner } from "../../../../hooks/useSpinner";
 import Button from "../../../atoms/Buttons/Button";
 import Icon from "../../../atoms/Icons/Icons";
 import LinkBasic from "../../../atoms/Link/LinkBasic";
 import LinkContainer from "../../../atoms/Link/LinkContainer";
 import AuthFlow from "../../../molecules/Modal/Auth/AuthFlow";
-import ForgotPasswordModal from "../../../molecules/Modal/Auth/ForgotPasswordModal";
 import ProfileModal from "../../../molecules/Modal/Auth/ProfileModal";
 import RequiredAuthModal from "../../../molecules/Modal/Auth/RequiredAuthModal";
 import NavLinksContainer from "./NavLinksContainer";
