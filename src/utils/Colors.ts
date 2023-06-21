3ccd1c78d2df2f6cd1497bcadc0fc1f21e0743d5
DESKTOP-AGFU7TK\Pablo <pabloriusblanco@gmail.com>
2023-06-21 15:36:40 -0300

    Fix - Adding Colors Ref 2/2

---
+++ src/utils/Colors.ts
@@ -0,0 +1,34 @@
+export const Colors = {
+  primary: {
+    light: "#F0CB63",
+    DEFAULT: "#F0BA23",
+    dark: "#DBAA2D",
+  },
+  secondary: {
+    light: "#D1D5DB",
+    DEFAULT: "#6B7280",
+    dark: "#4B5563",
+  },
+  danger: {
+    light: "#FECACA",
+    DEFAULT: "#EF4444",
+    dark: "#B91C1C",
+  },
+  success: {
+    light: "#A7F3D0",
+    DEFAULT: "#10B981",
+    dark: "#047857",
+  },
+  black: {
+    light: "#9CA3AF",
+    DEFAULT: '#4D4D4D',
+    dark: "#1F2937",
+  },
+  gray: {
+    light: "#F2F2F2",
+    DEFAULT: '#A9A9A9',
+    dark: "#8B8B8B",
+  },
+};
+
+export type ColorTypes = keyof typeof Colors;
